74bb44cf8899104688e140b45d4684c080ae9f87
Kristi <kristithecoder@gmail.com>
2017-11-13 09:20:23 -0800

    adding tests

---
+++ BookTracker/Models/DataModels/MyUserManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTracker.Models.DataModels
+{
+    public class MyUserManager<TUser>  :UserManager<TUser> where TUser : class
+    {
+        public MyUserManager(IUserStore<TUser> userStore, Microsoft.Extensions.Options.IOptions<IdentityOptions>options, IPasswordHasher<TUser> passwordHasher, IEnumerable<IUserValidator<TUser>> userValidators,IEnumerable<IPasswordValidator<TUser>> passwordValidators, ILookupNormalizer lookupNormalizer) : base(userStore,options,passwordHasher,userValidators,lookupNormalizer) { }
+    }
+}
---
+++ BookTrackerTests/BookControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using BookTracker.Services;
+using Moq;
+
+using Xunit;
+
+namespace BookTrackerTests
+{
+    public class BookControllerTests
+    {
+        [Fact]
+        public void LibraryReturnsListBookView()
+        {
+            var mockService = new Mock<IBookService>();
+            mockService.Setup(x=>x.GetAllBooks(It.IsAny))
+        }
+    }
+}
